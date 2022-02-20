9d86774377bab2bf3cda9dc7767d679492ca4ca1
glwhappen <1597721684@qq.com>
2022-02-20 23:26:35 +0800

    优化：减少eslint检查力度

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 'off', // 函数括号前必须有空格
+    'comma-dangle': 'off', // 数组多个对象后允许逗号
+    'keyword-spacing': 'off', // 关键字，例如：if后面是否强制空格
   }
 }
